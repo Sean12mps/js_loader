37239b8f02955c422847bb924a6dfdf63357c824
viankakrisna <viankakrisna@gmail.com>
2015-05-14 14:58:57 +0700

    create toggle to view starter code

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function( $ ) {
 	
+	$('pre').hide();
+
+	$('p + p').css({
+		cursor: 'pointer'
+	}).html('View starter code').on('click', function(){
+		$( this ).next().fadeToggle();
+	})
 });
