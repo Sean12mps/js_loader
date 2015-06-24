587dcde7060a3e7e8b1e1d22d881f54ae23329f7
Sean Michael <sean@calibreworks.co.id>
2015-06-25 01:51:52 +0700

    endpoints

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function( $ ) {
+jQuery( document ).ready( function( $ ) {
 	
 	$('.jsl-example').hide();
 
@@ -8,4 +8,26 @@ jQuery(document).ready(function( $ ) {
 		$( this ).next('.jsl-example').fadeToggle();
 	})
 
-});
+	$.ajax( {
+	    // url: "http://endpoint.cl0ckw0rks.com/?json=get_recent_posts",
+	    url: "http://endpoint.cl0ckw0rks.com/js-loader",
+	 
+	    // The name of the callback parameter, as specified by the YQL service
+	    jsonp: "callback",
+	 
+	    // Tell jQuery we're expecting JSONP
+	    dataType: "jsonp",
+	 
+	    // Tell YQL what we want and that we want JSON
+	    data: {
+	        cmd: "get_cdn_list",
+	        format: "json"
+	    },
+	 
+	    // Work with the response
+	    success: function( response ) {
+	        console.log( response ); // server response
+	    }
+	} );
+
+} );
